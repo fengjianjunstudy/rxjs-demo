aa74174dcc341fa62f9fa91a8174dc4dbaa3ab25
冯建俊 <fengjianjun@fengjianjundeMacBook-Pro.local>
2018-06-12 21:00:54 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import {fromEvent} from 'rxjs'
+console.log(fromEvent)
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      msg: 'hello rxjs'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
